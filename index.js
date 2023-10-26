dcbd36747f54d329e2a7109558e3e790cdc162d0
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-10-26 15:29:08 -0500

    Add functions to write input to generated README file and initialize app

--- index.js
+++ index.js
@@ -68,10 +68,41 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  const { title, description, installation, usage, contribution, test, license, username, email } = data;
+
+  const badgeSect = badge;
+
+  const titleSect = `# ${title}`;
+
+  const descripSect = `## Description\n\n${description}`;
+  
+  const installSect = `## Installation\n\n${installation}`;
+  
+  const usageSect = `## Usage\n\n${usage}`;
+  
+  const contribSect = `## Contributing\n\n${contribution}`;
+  
+  const testSect = `## Tests\n\n${test}`
+
+  const licenseSect = `## License\n\nThis application is covered under the ${license}.\n\nPlease refer to the LICENSE file in repo.`
+
+  const questionsSect = `## Questions\n\nMy GitHub profile: https://github.com/${username}\n\nIf you have any further questions, contact me through email.\n\n${email}`
+
+  const readme = `${badgeSect}\n\n${titleSect}\n\n${descripSect}\n\n${installSect}\n\n${usageSect}\n\n${contribSect}\n\n${testSect}\n\n${licenseSect}\n\n${questionsSect}`
+
+  fs.appendFile(fileName, readme, (err) => {
+    err ? console.error(err) : console.log('Your README file has been generated under the name "Generated_README.md"!')
+  })
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer.prompt(questions)
+  .then((answers) => {
+    writeToFile('Generated_README.md', answers);
+  });
+};
 
 // Function call to initialize app
 init();
